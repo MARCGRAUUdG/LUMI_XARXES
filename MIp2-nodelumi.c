7930965f9789e0c8e255fe2cf0fa475680a16349
MARCGRAUUdG <u1939685@campus.udg.edu>
2020-01-03 18:25:28 +0100

    Node LUMI fet.

--- MIp2-nodelumi.c
+++ MIp2-nodelumi.c
@@ -11,6 +11,7 @@
 /* Inclusió de llibreries, p.e. #include <stdio.h> o #include "meu.h"     */
 
 #include "lumiS.h"
+#include <stdbool.h>
 
 /* Definició de constants, p.e., #define XYZ       1500                   */
 
@@ -21,10 +22,38 @@
 
 int main(int argc,char *argv[])
 {
- /* Declaració de variables, p.e., int n;                                 */
+   /* Declaració de variables, p.e., int n;                                 */
+   FILE *cfg;
+   char domini[100/*assignat pel protocol?*/], entrada_t[300];
+   int nombreClients, llistaSockets[2], nBytes, canal, log;
+   bool final = false;
+   /* Expressions, estructures de control, crides a funcions, etc.          */
 
- /* Expressions, estructures de control, crides a funcions, etc.          */
+   cfg = fopen("MIp2-nodelumi.cfg", "r"); //r per poder llegir i escriure.
+   fscanf(cfg, "%s", domini);
+   fscanf(cfg, "%d", domini);
+   struct adrUDP taulaClients[nClients];
+   log = crearLogServidor(nomDomini);
 
+   llistaSockets[0] = 0;
+   llistaSockets[1] = LUMI_S_Inicialitzar(domini, nombreClients, cfg, taulaClients); //mirar com es crida
+
+   printf("# per acabar");
+
+   while (!final)
+   {
+     canal = LUMI_C_HaArribatAlgunaCosa(llistaSockets, -1, 2); //mirar com es crida
+     if (canal == 0)
+     {
+       nBytes = read(0,teclat,sizeof(teclat));
+  		 if(teclat[0] == '#') fi = true;
+     } else
+     {
+       LUMI_S_ServeixPeticio(Sck, domini, taulaClients, nombreClients, log); //mirar com es crida
+     }
+   }
+   tancaLog(log);
+   return 0;
  }
 
 /* Definició de funcions INTERNES, és a dir, d'aquelles que es faran      */
